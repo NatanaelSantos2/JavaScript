b12f14d67418f615d08064269f6fc57fc29b9db5
NatanaelSantos2 <154018545+NatanaelSantos2@users.noreply.github.com>
2023-12-24 11:36:52 -0300

    Add files via upload

---
+++ ex004/tiposTriangulo.js
@@ -0,0 +1,15 @@
+let a = 1;
+let b = 2;
+let c = 5;
+
+soma = a + b
+
+if(a === b && a === c){
+    console.log("Equilátero");
+} else if(soma > c && soma > c && a !== b && a !== b && c !== b){
+    console.log("Escaleno");
+}else if(a === b || a === c || b === c){
+    console.log("Isósceles")
+}else{
+    console.log("Não é um triângulo")
+}
