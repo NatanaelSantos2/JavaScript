52af667d116b22d93b0bacbb4c869c9e36aec56d
NatanaelSantos2 <154018545+NatanaelSantos2@users.noreply.github.com>
2023-12-25 10:06:35 -0300

    Add files via upload

---
+++ ex005/vogais.js
@@ -0,0 +1,10 @@
+var letra = 'a';
+
+switch(letra){
+    case 'a': console.log("Vogais"); break;
+    case 'e': console.log("Vogais"); break;
+    case 'i': console.log("Vogais"); break;
+    case 'o': console.log("Vogais"); break;
+    case 'u': console.log("Vogais"); break;
+    default: console.log("Consoantes");
+}
