2a733b99b367889c42f838e0db32c2597d87b134
NatanaelSantos2 <154018545+NatanaelSantos2@users.noreply.github.com>
2023-12-31 00:16:07 -0300

    Add files via upload

---
+++ Exercícios/ex011/contarNumero.js
@@ -0,0 +1,28 @@
+/* cont = 1
+
+while(cont <= 10){
+    console.log(cont);
+    cont++
+} */
+
+
+// Forma de contar ate 10
+// com for
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+}
+
+// Com while
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+// Com do while
+// let contador = 1;
+do {
+    console.log(contador);
+    contador++;
+} while (contador < 10)
