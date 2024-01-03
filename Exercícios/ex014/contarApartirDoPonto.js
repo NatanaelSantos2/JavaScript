d6aac7ec4b1bf1251864e301e20ce825fa42842c
NatanaelSantos2 <154018545+NatanaelSantos2@users.noreply.github.com>
2024-01-03 05:36:16 -0300

    Add files via upload

---
+++ Exercícios/ex014/contarApartirDoPonto.js
@@ -0,0 +1,12 @@
+var inicio = 5;
+var fim = 2;
+
+if (inicio > fim) {
+    for (let i = inicio; i >= fim; i--) {
+        console.log(i);
+    }
+} else {
+    for (let i = inicio; i <= fim; i++) {
+        console.log(i);
+    }
+}
