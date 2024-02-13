462d21e75968374c254228a31a09e5545e5cc431
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-02-13 18:18:29 -0300

    Add files via upload

---
+++ Exercícios/ex041/script.js
@@ -0,0 +1,17 @@
+function primo(numero) {
+    if (numero < 2) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function main(numero){
+    console.log(primo(numero));
+}
+
+main(2);
