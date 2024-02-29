a7a29bae5d4188fe26f62932bb2a5bc13508cbb1
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-02-29 16:30:50 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/array/join/script.js
@@ -0,0 +1,6 @@
+const frutas = ["maçã", "mamão", "laranja", "banana"];
+
+console.log(frutas.join());
+console.log(frutas.join(" "));
+console.log(frutas.join(" - "));
+console.log(frutas.join(", "));
