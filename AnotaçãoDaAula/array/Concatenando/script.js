a9b30ca1a09705b27899fda2f24b8312622fb639
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-02 19:49:08 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/array/Concatenando/script.js
@@ -0,0 +1,7 @@
+const animais = ['bufalo', 'camelo', 'pato', 'elefante'];
+const frutas = ["maçã", "mamão", "laranja", "banana"];
+
+// const array = [].concat(animais).concat(frutas);
+const array = animais.concat(['zebra', 'onça']);
+
+console.log(array);
