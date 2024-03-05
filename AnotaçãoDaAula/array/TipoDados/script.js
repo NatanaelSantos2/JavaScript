970a6a3a4fb4f45bede5820785b9763e3c82f792
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-04 22:16:04 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/array/TipoDados/script.js
@@ -0,0 +1,14 @@
+//Tipos de dados em um array
+[120, 'Nael', {}, [], function(){}]
+
+// Pelo construtor do array
+const array1 = new Array(3); // 1 argumento
+// [,,]
+
+// 2 ou mais argumentos
+const array = new Array(3, 5);
+// console.log(array);
+
+// Pela notação literal
+const forma2 = [3];
+console.log(forma2);
