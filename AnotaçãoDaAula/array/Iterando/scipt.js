48cba9aab0c2e7c3021fdc3254c138c25af4cc6f
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-06 14:41:16 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/array/Iterando/scipt.js
@@ -0,0 +1,20 @@
+const frutas = ["maçã", "mamão", "laranja", "banana"];
+
+for(let i = 1; i < frutas.length; i++) {
+    const nomeFruta = frutas[i];
+    const indiceFruta = frutas.indexOf(nomeFruta);
+    console.log(`${indiceFruta}-${nomeFruta}`);
+} 
+
+frutas.forEach(function(fruta, indice, frutas) {
+    //console.log(`${indice}-${frutas}`);
+    console.log(frutas);
+})
+
+for(const indice in frutas) {
+    console.log(indice);
+}
+
+for(const nome of frutas) {
+    console.log(nome);
+}
