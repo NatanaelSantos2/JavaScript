d64b351a8324a29f9459e3a8e0f0c8a0b6ebd390
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-07 20:34:20 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/array/ManipulandoElement/script.js
@@ -0,0 +1,15 @@
+const frutas = ["maçã", "banana", "laranja"];
+
+console.log(frutas[0]);
+
+const resultado = frutas.indexOf("maçã");
+frutas[resultado] = "uva";
+console.log(frutas);
+
+
+//Outro exemplo
+/* const frutas = ["maçã", "mamão", "laranja", "banana"];
+
+frutas.unshift("coco");
+console.log(frutas.push("morango"));
+ */
