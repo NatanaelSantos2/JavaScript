b02aa8f3e92d33858c7aa4811fae3c3b77069750
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-12 21:14:55 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/array/Reduzindo/script.js
@@ -0,0 +1,27 @@
+const numeros = [5, 6, 3, 1, 4];
+
+const resultado = numeros.reduce(function (acumulador, valorAtual) {
+    //console.log(acumulador, valorAtual);
+    return acumulador + valorAtual;
+}, 0);
+
+console.log(resultado);
+
+const cursos = [
+    {
+        nome: 'JavaScript',
+        videos: 10,
+    },
+    {
+        nome: 'PHP',
+        videos: 3,
+    },
+    {
+        nome: 'PYTHON',
+        videos: 35,
+    }
+]
+
+const totalVideos = cursos.reduce((acumulador, curso) => acumulador + curso.videos, 0);
+
+console.log(`O total de videos é ${totalVideos}`)
