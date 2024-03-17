e55f528e8d60736b37102ec6d83993cb24fc260b
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-17 20:18:58 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/DataEHora/data/date.js
@@ -0,0 +1,7 @@
+const data1 = new Date();
+const data2 = new Date("December 17, 2022, 07:00:23.001z");
+const data3 = new Date("2024-11-13T13:15:01z");
+var mes = 5
+const data4 = new Date(2020, mes - 1, 10, 11 -3, 13, 15);
+const data5 = new Date.UTC(2020, mes -1, 10, 11, 13, 15);
+const data6 = new Date(data5);
