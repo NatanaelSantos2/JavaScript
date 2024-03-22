1d7799176b31bb3893d95d29f24431b21359db0a
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-21 22:33:44 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/DataEHora/data/UTC.js
@@ -0,0 +1,6 @@
+const comUTC = new Date('2023-04-16T12:00:00.000Z');
+const semUTC = new Date('2023-04-16T12:00:00.000');
+const data = new Date('2023-04-16T12:00:00.000-05:00');
+console.log(comUTC);
+console.log(semUTC);
+console.log(data);
