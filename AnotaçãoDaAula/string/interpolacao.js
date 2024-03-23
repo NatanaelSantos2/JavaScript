b8a005ea9f203316f4d5d76fceba77c0528be1b7
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-22 21:16:37 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/string/interpolacao.js
@@ -0,0 +1,7 @@
+// Template String e interpolação
+
+console.log(`Nael falou para \`falar fazer: "..."`);
+
+const nome = "Nael";
+const sobreNome = "Santos";
+console.log(`${nome} ${sobreNome} idade: ${2024 - 1999}`);
