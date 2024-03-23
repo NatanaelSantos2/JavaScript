b396b923428bac605282e43eb46531e02770fe30
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-23 17:28:30 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/string/concatenacaoEescape.js
@@ -0,0 +1,15 @@
+// Concatenação de strings e escape de caracteres
+
+const nome = "Nael ";
+const sobreNome = "Santos";
+
+console.log(nome + sobreNome + ' Idade: ' + (2024 - 1999));
+
+console.log('Caixa d\'água');
+console.log("Nael falou: \"...\"");
+
+console.log('Nael \nSantos'); // quebra linha
+
+console.log("Nael\tsantos"); // dá um tab
+
+console.log("Nael\\santos"); // mostra \
