65a9ce463ce0c66db75c8f2ad0e98d6d2217e3ea
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-03-28 21:41:33 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/Funções/use strict.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const myFucn = () => {
+    let nome = "Nael";
+    console.log("Ola", nome);
+}
+
+myFucn();
+// console.log(nome);
+
+function calculador(x, y) {
+    console.log(x+ y);
+}
+
+calculador(10, 5);
