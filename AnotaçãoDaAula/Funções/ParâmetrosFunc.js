33861349f6b8d37b457d8b48c17083ad13b49341
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-04-10 22:32:30 -0300

    Update ParâmetrosFunc.js

--- AnotaçãoDaAula/Funções/ParâmetrosFunc.js
+++ AnotaçãoDaAula/Funções/ParâmetrosFunc.js
@@ -4,7 +4,7 @@ function calculador(valorX, valorY){
 
 calculador(5, 10); // Saída: 15
 
-//Atividade2
+//Atividade2 Parâmetros com valor default
 function calculador2(valorX, valorY = 10) {
     console.log(valorX, valorY);
 }
