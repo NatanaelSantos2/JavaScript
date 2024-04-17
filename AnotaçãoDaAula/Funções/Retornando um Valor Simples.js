066c869a7074b8c3bd49715244357da1bbdecff3
Natanael Santos <154018545+NatanaelSantos2@users.noreply.github.com>
2024-04-16 21:12:58 -0300

    Add files via upload

---
+++ AnotaçãoDaAula/Funções/Retornando um Valor Simples.js
@@ -0,0 +1,8 @@
+function soma(a, b) {
+    console.log(a + b);
+    return "Ola mundo!";
+}
+
+const resultado = soma(3, 5);
+console.log(resultado); // Saída: 8
+                        // "Ola mundo!"
